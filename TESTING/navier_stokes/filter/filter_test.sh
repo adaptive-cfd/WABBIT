1ca505d48493d5fcbaa73aab81c4c4045be420fd
Philipp137 <Philipp.l.krah@gmail.com>
2018-04-19 15:27:58 +0200

    [NS] filter test added

---
+++ TESTING/navier_stokes/filter/filter_test.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# WABBIT unit test
+# This file contains one specific unit test, and it is called by unittest.sh
+#-------------------------------------------------------------------------------
+# what parameter file
+test_dir="./TESTING/"
+dir="./TESTING/navier_stokes/filter/"
+params=${dir}"sod_shock_tube.ini"
+happy=0
+sad=0
+echo "testing filters"
+
+# list of prefixes the test generates
+prefixes=(Ux p rho)
+# list of filters
+filters=(no_filter explicit_5pt explicit_7pt explicit_9pt explicit_11pt wavelet bogey_shock)
+## change this list in run_all_filter.sh to!!
+# list of possible times (no need to actually have them)
+times=(000000050000)
+
+
+
+
+${dir}/run_all_filter.sh
+
+
+	echo "============================"
+	echo "run done, analyzing data now"
+	echo "============================"
+
+for filter in ${filters[@]}
+do
+	# loop over all HDF5 files and generate keyvalues using wabbit
+	for p in ${prefixes[@]}
+	do
+	  for t in ${times[@]}
+	  do
+	    echo "--------------------------------------------------------------------"
+	    #configname
+	    configname=${filter}_${p}"_"${t}
+	    # *.h5 file coming out of the code
+	   	file=${configname}".h5"
+	    # will be transformed into this *.key file
+	    keyfile=${configname}".key"
+	    # which we will compare to this *.ref file
+	    reffile=${dir}/${configname}".ref"
+	    if [ -f $file ]; then
+	        # get four characteristic values describing the field
+	        ${mpi_command} ./wabbit-post 2D --keyvalues ${file}
+	        # and compare them to the ones stored
+	        if [ -f $reffile ]; then
+	        	${mpi_command} ./wabbit-post 2D --compare-keys $keyfile $reffile
+	            result=$(cat return); rm return
+	            if [ $result == "0" ]; then
+	              echo -e ":) Happy, this looks okay!" $keyfile $reffile
+	              happy=$((happy+1))
+	            else
+	              echo -e ":[ Sad, this is failed!" $keyfile $reffile
+	              sad=$((sad+1))
+	            fi
+	        else
+	            sad=$((sad+1))
+	            echo -e ":[ Sad: Reference file not found"
+	        fi
+	    else
+	        sad=$((sad+1))
+	        echo -e ":[ Sad: output file not found"
+	    fi
+	    echo " "
+	    echo " "
+
+	  done
+	done
+done
+
+echo -e "\t happy tests:\t" $happy
+echo -e "\t sad tests: \t" $sad
+
+#-------------------------------------------------------------------------------
+#                               RETURN
+#-------------------------------------------------------------------------------
+if [ $sad == 0 ]
+then
+  exit 0
+else
+  exit 999
+fi
---
+++ TESTING/navier_stokes/filter/run_all_filter.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# This routine runs all filters on sods shock tube in the list of available filters 
+# and produces data (statevector) at time t=0.05 for comparing the filters
+# author: P.Krah date: 20.04.2018
+#-------------------------------------------------------------------------------
+
+test_dir="./TESTING/"
+dir="./TESTING/navier_stokes/filter/"
+params=${dir}"filter_test.ini"
+# list of prefixes the test generates
+prefixes=(Ux p rho)
+# list of filters
+filters=(no_filter explicit_5pt explicit_7pt explicit_9pt explicit_11pt wavelet bogey_shock)
+
+# list of possible times (no need to actually have them)
+times=(000000050000)
+
+
+for filter in ${filters[@]}
+	do
+
+	echo " "
+	echo " "
+	echo "===================================================================="
+	echo -e "running filter:" ${filter} 
+	echo "===================================================================="
+	echo " "
+	echo " "
+	echo " "
+
+	./${test_dir}/replace_ini_value.sh  ${params} filter_type ${filter}
+	# run actual test
+	${mpi_command} ./wabbit 2D ${params} --memory=2GB
+
+	echo " "
+	echo " "
+	echo " "
+
+	# loop over all HDF5 files and generate keyvalues using wabbit
+	for p in ${prefixes[@]}
+	do
+	  for t in ${times[@]}
+	  do
+	    #current configname
+	    configname=${p}"_"${t}
+	    #change configname for current filter
+	    cp ${configname}".h5" ${filter}_${configname}".h5"
+	    #
+	    echo "changed: " ${configname}".h5 to" ${filter}_${configname}".h5"
+	  done
+	done
+	echo " "
+	echo " "
+	echo " "
+	echo " "
+done
+
+exit 0

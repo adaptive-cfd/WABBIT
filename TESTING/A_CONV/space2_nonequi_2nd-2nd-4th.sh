db9e7e86c330ece9483e83cd41107c574c68586d
Thomas Engels <thomas.engels@mailbox.tu-berlin.de>
2018-04-20 07:29:31 +0200

    minor changes in testing

--- TESTING/A_CONV/space2_nonequi_2nd-2nd-4th.sh
+++ TESTING/A_CONV/space2_nonequi_2nd-2nd-4th.sh
@@ -38,8 +38,8 @@ do
 	../replace_ini_value.sh $ini Blocks min_treelevel 2
 
 	# other
-	../replace_ini_value.sh $ini nu 0.0 ConvectionDiffusion
-	../replace_ini_value.sh $ini blob_width 0.01 ConvectionDiffusion
+	../replace_ini_value.sh $ini ConvectionDiffusion nu 0.0
+	../replace_ini_value.sh $ini ConvectionDiffusion blob_width 0.01 
 
 	$mpi ./wabbit 2D $ini --memory=3.0GB
 	cd ..

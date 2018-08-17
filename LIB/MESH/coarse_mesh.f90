af9a28da19ea843effd59cb4e09fbc60b5b2b0cd
Thomas Engels <thomas.engels@mailbox.tu-berlin.de>
2018-08-17 18:37:28 +0200

    soft-revert philipps work on non-blocking gathering. code commented out.

--- LIB/MESH/coarse_mesh.f90
+++ LIB/MESH/coarse_mesh.f90
@@ -38,11 +38,10 @@ subroutine coarse_mesh( params, lgt_block, hvy_block, lgt_active, lgt_n, lgt_sor
     ! loop variables
     integer(kind=ik)                    :: k, maxtl, N
     ! list of block ids, proc ranks
-    integer(kind=ik)                    :: light_ids(1:8,lgt_n)
+    integer(kind=ik)                    :: light_ids(1:8)
     ! rank of proc to keep the coarsened data
-    integer(kind=ik)                    :: data_rank,  n_req, ierr,n_merge
-    ! non blocking send receives use communication requests
-    integer(kind=ik),save,allocatable   :: request(:)
+    integer(kind=ik)                    :: data_rank
+
 
 !---------------------------------------------------------------------------------------------
 ! variables initialization
@@ -55,12 +54,9 @@ subroutine coarse_mesh( params, lgt_block, hvy_block, lgt_active, lgt_n, lgt_sor
       N = 4
     endif
 
-    ! at worst every block is on a different rank
-    if (.not. allocated(request)) allocate(request(N*size(lgt_block,1)))
 !---------------------------------------------------------------------------------------------
 ! main body
-    n_req   = 0_ik
-    n_merge = 0_ik
+
     ! loop over all active light data
     do k = 1, lgt_n
       ! FIRST condition: only work on light data, if block is active. Usually, you would do that just with
@@ -71,33 +67,117 @@ subroutine coarse_mesh( params, lgt_block, hvy_block, lgt_active, lgt_n, lgt_sor
       ! SECOND condition: block wants to coarsen, i.e. it has the status -1. Note the routine
       ! ensure_gradedness removes the -1 flag if not all sister blocks share it (NOTE: Before 06/2018, we
       ! used a second status: -2 to mark blocks that can *really* be coarsened)
-      if ( lgt_block(lgt_active(k), 1) >= 0 .and. lgt_block(lgt_active(k), maxtl+idx_refine_sts) == -1) then
-          ! count the number of merges
-          n_merge=n_merge+1
+      if ( lgt_block(lgt_active(k), 1) >= 0 .and. lgt_block(lgt_active(k), maxtl+2) == -1) then
           ! Check which CPU holds this block. The CPU will also hold the merged, new block
           call lgt_id_to_proc_rank( data_rank, lgt_active(k), params%number_blocks )
 
           ! find all sisters (including the block in question, so four blocks)
           ! their light IDs are in "light_ids" and ordered by their last treecode-digit
-          call find_sisters( params, lgt_active(k), light_ids(1:N,n_merge), lgt_block, lgt_n, lgt_sortednumlist )
+          call find_sisters( params, lgt_active(k), light_ids(1:N), lgt_block, lgt_n, lgt_sortednumlist )
 
           ! gather all four sisters on the process "datarank". The light_ids are updated in the routine
           ! and they are still in the same order (0,1,2,3)-sister. It is just that they are now on one CPU
-          call gather_blocks_on_proc( params, hvy_block, lgt_block, data_rank, light_ids(1:N,n_merge), request, n_req )
-        endif
-      end do
+          call gather_blocks_on_proc( params, hvy_block, lgt_block, data_rank, light_ids(1:N) )
 
-      ! In gather_block_on_proc we have initiated communications with irecv and isend, to get the
-      ! block information for sisters on different ranks.
-      ! Before the sisters can be merged we have to make sure, that sending and receiving is done!
-      if (n_req > 0_ik ) call MPI_Waitall( n_req, request, MPI_STATUSES_IGNORE, ierr)
-
-      ! After we have gathered all n_merge*N sister blocks, we merge them into n_merge parent blocks
-      do k = 1, n_merge
           ! merge the four blocks into one new block. Merging is done in two steps,
           ! first for light data (which all CPUS do redundantly, so light data is kept synched)
           ! Then only the responsible rank will perform the heavy data merging.
-          call merge_blocks( params, hvy_block, lgt_block, light_ids(1:N,k) )
-      enddo
+          call merge_blocks( params, hvy_block, lgt_block, light_ids(1:N) )
+        endif
+    end do
+
 
 end subroutine coarse_mesh
+
+
+
+!
+! subroutine coarse_mesh( params, lgt_block, hvy_block, lgt_active, lgt_n, lgt_sortednumlist )
+!
+! !---------------------------------------------------------------------------------------------
+! ! modules
+!
+! !---------------------------------------------------------------------------------------------
+! ! variables
+!
+!     implicit none
+!
+!     !> user defined parameter structure
+!     type (type_params), intent(in)      :: params
+!     !> light data array
+!     integer(kind=ik), intent(inout)     :: lgt_block(:, :)
+!     !> heavy data array - block data
+!     real(kind=rk), intent(inout)        :: hvy_block(:, :, :, :, :)
+!     !> list of active blocks (light data)
+!     integer(kind=ik), intent(inout)     :: lgt_active(:)
+!     !> number of active blocks (light data)
+!     integer(kind=ik), intent(inout)     :: lgt_n
+!     !> sorted list of numerical treecodes, used for block finding
+!     integer(kind=tsize), intent(inout)  :: lgt_sortednumlist(:,:)
+!
+!     ! loop variables
+!     integer(kind=ik)                    :: k, maxtl, N
+!     ! list of block ids, proc ranks
+!     integer(kind=ik)                    :: light_ids(1:8,lgt_n)
+!     ! rank of proc to keep the coarsened data
+!     integer(kind=ik)                    :: data_rank,  n_req, ierr,n_merge
+!     ! non blocking send receives use communication requests
+!     integer(kind=ik),save,allocatable   :: request(:)
+!
+! !---------------------------------------------------------------------------------------------
+! ! variables initialization
+!
+!     maxtL = params%max_treelevel
+!
+!     if (params%threeD_case) then
+!       N = 8
+!     else
+!       N = 4
+!     endif
+!
+!     ! at worst every block is on a different rank
+!     if (.not. allocated(request)) allocate(request(N*size(lgt_block,1)))
+! !---------------------------------------------------------------------------------------------
+! ! main body
+!     n_req   = 0_ik
+!     n_merge = 0_ik
+!     ! loop over all active light data
+!     do k = 1, lgt_n
+!       ! FIRST condition: only work on light data, if block is active. Usually, you would do that just with
+!       ! the active list. NOTE HERE: due to previous loops, some light data id are already
+!       ! coarsened, (and thus given the -1) but they are still in active block list
+!       ! so: check again if this block is REALLY active (the active list is updated later)
+!       !
+!       ! SECOND condition: block wants to coarsen, i.e. it has the status -1. Note the routine
+!       ! ensure_gradedness removes the -1 flag if not all sister blocks share it (NOTE: Before 06/2018, we
+!       ! used a second status: -2 to mark blocks that can *really* be coarsened)
+!       if ( lgt_block(lgt_active(k), 1) >= 0 .and. lgt_block(lgt_active(k), maxtl+idx_refine_sts) == -1) then
+!           ! count the number of merges
+!           n_merge=n_merge+1
+!           ! Check which CPU holds this block. The CPU will also hold the merged, new block
+!           call lgt_id_to_proc_rank( data_rank, lgt_active(k), params%number_blocks )
+!
+!           ! find all sisters (including the block in question, so four blocks)
+!           ! their light IDs are in "light_ids" and ordered by their last treecode-digit
+!           call find_sisters( params, lgt_active(k), light_ids(1:N,n_merge), lgt_block, lgt_n, lgt_sortednumlist )
+!
+!           ! gather all four sisters on the process "datarank". The light_ids are updated in the routine
+!           ! and they are still in the same order (0,1,2,3)-sister. It is just that they are now on one CPU
+!           call gather_blocks_on_proc( params, hvy_block, lgt_block, data_rank, light_ids(1:N,n_merge), request, n_req )
+!         endif
+!       end do
+!
+!       ! In gather_block_on_proc we have initiated communications with irecv and isend, to get the
+!       ! block information for sisters on different ranks.
+!       ! Before the sisters can be merged we have to make sure, that sending and receiving is done!
+!       if (n_req > 0_ik ) call MPI_Waitall( n_req, request, MPI_STATUSES_IGNORE, ierr)
+!
+!       ! After we have gathered all n_merge*N sister blocks, we merge them into n_merge parent blocks
+!       do k = 1, n_merge
+!           ! merge the four blocks into one new block. Merging is done in two steps,
+!           ! first for light data (which all CPUS do redundantly, so light data is kept synched)
+!           ! Then only the responsible rank will perform the heavy data merging.
+!           call merge_blocks( params, hvy_block, lgt_block, light_ids(1:N,k) )
+!       enddo
+!
+! end subroutine coarse_mesh
--- LIB/MESH/gather_blocks_on_proc.f90
+++ LIB/MESH/gather_blocks_on_proc.f90
@@ -12,7 +12,9 @@
 !!
 !! Uses non- blocking communication.
 ! ********************************************************************************************
-subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt_blocks_to_gather,request, n_req )
+
+
+subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt_blocks_to_gather )
   implicit none
 
   !> user defined parameter structure
@@ -26,12 +28,10 @@ subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt
   integer(kind=ik), intent(inout)     :: lgt_blocks_to_gather(:)
   !> on which MPIRANK will be gather the desired blocks?
   integer(kind=ik), intent(in)        :: gather_rank
-  integer(kind=ik), intent(inout)     :: request(:), n_req
 
   integer(kind=ik) :: i, hvy_id, owner_rank, myrank, npoints
   integer(kind=ik) :: lgt_free_id, tag, hvy_free_id, ierr
-  ! MPI request
-
+  integer :: status(MPI_STATUS_SIZE)
 
   myrank = params%rank
 
@@ -46,7 +46,7 @@ subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt
           ! use a unique tag for message exchange
           tag = lgt_blocks_to_gather(i)
 
-          ! get a free place to gather the block ("lgt_free_id") on the gather_rank
+          ! get a free place to gather the block ("lgt_free_id") on the agather_rank
           ! for light data synching, it is good to have all procs do that
           call get_free_local_light_id( params, gather_rank, lgt_block, lgt_free_id)
 
@@ -57,12 +57,9 @@ subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt
               !------------------------
               ! get hvy id where to store the data
               call lgt_id_to_hvy_id( hvy_free_id, lgt_free_id, myrank, params%number_blocks )
+
               npoints = size(hvy_block,1)*size(hvy_block,2)*size(hvy_block,3)*size(hvy_block,4)
-              ! increment the list of requests
-              n_req  = n_req + 1
-              request(n_req) = MPI_REQUEST_NULL
-              call MPI_irecv( hvy_block(:,:,:,:,hvy_free_id), npoints, MPI_REAL8, owner_rank, &
-                              tag, WABBIT_COMM, request(n_req), ierr)
+              call MPI_recv( hvy_block(:,:,:,:,hvy_free_id), npoints, MPI_REAL8, owner_rank, tag, WABBIT_COMM, status, ierr)
 
           elseif ( myrank == owner_rank) then
               ! Am I the owner of this block, so will I have to send data?
@@ -73,12 +70,12 @@ subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt
               call lgt_id_to_hvy_id( hvy_id, lgt_blocks_to_gather(i), owner_rank, params%number_blocks )
 
               npoints = size(hvy_block,1)*size(hvy_block,2)*size(hvy_block,3)*size(hvy_block,4)
-              ! increment the list of requests
-              n_req = n_req + 1
-              request(n_req) = MPI_REQUEST_NULL
-              call MPI_isend( hvy_block(:,:,:,:,hvy_id), npoints, MPI_REAL8, gather_rank, tag, &
-                              WABBIT_COMM, request(n_req), ierr)
+              call MPI_send( hvy_block(:,:,:,:,hvy_id), npoints, MPI_REAL8, gather_rank, tag, WABBIT_COMM, ierr)
+
+              ! delete old heavy data
+              hvy_block(:,:,:,:,hvy_id) = 4.0e11_rk
           endif
+
           ! even if I am not concerned with sending or recv, the light data changes, assuming the copy went through.
           ! if it did not, code hangs anyways. so here assume it worked and on all CPU just copy the light data
           lgt_block( lgt_free_id, : ) = lgt_block( lgt_blocks_to_gather(i), : )
@@ -87,9 +84,92 @@ subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt
           ! we return the new index of the blocks we moved
           lgt_blocks_to_gather(i) = lgt_free_id
       endif
-      ! make all gathered blocks inactive until they are merged
-      lgt_block( lgt_blocks_to_gather(i), params%max_treelevel + idx_refine_sts ) = 0
   enddo
 
 ! NOTE; even though light data is synched, active lists etc have to be created afterwards!
 end subroutine gather_blocks_on_proc
+
+
+
+
+! subroutine gather_blocks_on_proc( params, hvy_block, lgt_block, gather_rank, lgt_blocks_to_gather,request, n_req )
+!   implicit none
+!
+!   !> user defined parameter structure
+!   type (type_params), intent(in)      :: params
+!   !> light data array
+!   integer(kind=ik), intent(inout)     :: lgt_block(:, :)
+!   !> heavy data array - block data
+!   real(kind=rk), intent(inout)        :: hvy_block(:, :, :, :, :)
+!   !> list of blocks (lightIDs) to be gathered. on output, the new IDs of
+!   !! these blocks are returned.
+!   integer(kind=ik), intent(inout)     :: lgt_blocks_to_gather(:)
+!   !> on which MPIRANK will be gather the desired blocks?
+!   integer(kind=ik), intent(in)        :: gather_rank
+!   integer(kind=ik), intent(inout)     :: request(:), n_req
+!
+!   integer(kind=ik) :: i, hvy_id, owner_rank, myrank, npoints
+!   integer(kind=ik) :: lgt_free_id, tag, hvy_free_id, ierr
+!   ! MPI request
+!
+!
+!   myrank = params%rank
+!
+!   ! look at all blocks in the gather list
+!   do i = 1, size(lgt_blocks_to_gather)
+!       ! which mpirank owns the block?
+!       call lgt_id_to_proc_rank( owner_rank, lgt_blocks_to_gather(i), params%number_blocks )
+!
+!       ! if gather_rank == owner_rank, nothing to be done, as block is already on
+!       ! right processor
+!       if ( gather_rank /= owner_rank) then
+!           ! use a unique tag for message exchange
+!           tag = lgt_blocks_to_gather(i)
+!
+!           ! get a free place to gather the block ("lgt_free_id") on the gather_rank
+!           ! for light data synching, it is good to have all procs do that
+!           call get_free_local_light_id( params, gather_rank, lgt_block, lgt_free_id)
+!
+!           ! Am I the target rank who receives all the data?
+!           if ( myrank == gather_rank) then
+!               !------------------------
+!               ! RECV CASE
+!               !------------------------
+!               ! get hvy id where to store the data
+!               call lgt_id_to_hvy_id( hvy_free_id, lgt_free_id, myrank, params%number_blocks )
+!               npoints = size(hvy_block,1)*size(hvy_block,2)*size(hvy_block,3)*size(hvy_block,4)
+!               ! increment the list of requests
+!               n_req  = n_req + 1
+!               request(n_req) = MPI_REQUEST_NULL
+!               call MPI_irecv( hvy_block(:,:,:,:,hvy_free_id), npoints, MPI_REAL8, owner_rank, &
+!                               tag, WABBIT_COMM, request(n_req), ierr)
+!
+!           elseif ( myrank == owner_rank) then
+!               ! Am I the owner of this block, so will I have to send data?
+!               !------------------------
+!               ! SEND CASE
+!               !------------------------
+!               ! what heavy ID (on this proc) does the block have?
+!               call lgt_id_to_hvy_id( hvy_id, lgt_blocks_to_gather(i), owner_rank, params%number_blocks )
+!
+!               npoints = size(hvy_block,1)*size(hvy_block,2)*size(hvy_block,3)*size(hvy_block,4)
+!               ! increment the list of requests
+!               n_req = n_req + 1
+!               request(n_req) = MPI_REQUEST_NULL
+!               call MPI_isend( hvy_block(:,:,:,:,hvy_id), npoints, MPI_REAL8, gather_rank, tag, &
+!                               WABBIT_COMM, request(n_req), ierr)
+!           endif
+!           ! even if I am not concerned with sending or recv, the light data changes, assuming the copy went through.
+!           ! if it did not, code hangs anyways. so here assume it worked and on all CPU just copy the light data
+!           lgt_block( lgt_free_id, : ) = lgt_block( lgt_blocks_to_gather(i), : )
+!           ! we must also delete the original block (also done on ALL CPUS)
+!           lgt_block( lgt_blocks_to_gather(i), : ) = -1
+!           ! we return the new index of the blocks we moved
+!           lgt_blocks_to_gather(i) = lgt_free_id
+!       endif
+!       ! make all gathered blocks inactive until they are merged
+!       lgt_block( lgt_blocks_to_gather(i), params%max_treelevel + idx_refine_sts ) = 0
+!   enddo
+!
+! ! NOTE; even though light data is synched, active lists etc have to be created afterwards!
+! end subroutine gather_blocks_on_proc

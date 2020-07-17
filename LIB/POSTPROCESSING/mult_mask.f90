65a14bb65030458adc7056704295d44541c0b486
Dmitry Kolomenskiy <dkolom@gmail.com>
2020-07-17 10:53:17 +0900

    Update mult_mask.f90

--- LIB/POSTPROCESSING/mult_mask.f90
+++ LIB/POSTPROCESSING/mult_mask.f90
@@ -107,11 +107,6 @@ subroutine mult_mask(params)
 
     ! create lists of active blocks (light and heavy data)
     ! update list of sorted nunmerical treecodes, used for finding blocks
-    !call update_grid_metadata(params, lgt_block, hvy_neighbor, lgt_active, lgt_n, &
-    !    lgt_sortednumlist, hvy_active, hvy_n, tree_ID=1)
-
-    ! create lists of active blocks (light and heavy data)
-    ! update list of sorted nunmerical treecodes, used for finding blocks
     call create_active_and_sorted_lists( params, lgt_block, lgt_active, &
     lgt_n, hvy_active, hvy_n, lgt_sortednumlist, tree_ID=1)
     ! update neighbor relations

90490940a0e60ffd58d11851a0e3ca041d539d90
pkrah <philipp.l.krah@gmail.com>
2022-06-20 17:32:45 +0200

    statistic color!

--- LIB/EQUATION/ACMnew/statistics_ACM.f90
+++ LIB/EQUATION/ACMnew/statistics_ACM.f90
@@ -64,7 +64,7 @@ subroutine STATISTICS_ACM( time, dt, u, g, x0, dx, stage, work, mask )
     !   5           For the insects, this is 2ND RIGHT WING
     integer(kind=2), allocatable, save :: mask_color(:,:,:)
     integer(kind=2) :: color
-    logical :: is_insect
+    logical :: is_insect, use_color
 
     if (.not. params_acm%initialized) write(*,*) "WARNING: STATISTICS_ACM called but ACM not initialized"
 
@@ -85,7 +85,9 @@ subroutine STATISTICS_ACM( time, dt, u, g, x0, dx, stage, work, mask )
 
     ! save some computing time by using a logical and not comparing every time
     is_insect = .false.
+    use_color = .false.
     if (params_acm%geometry == "Insect") is_insect = .true.
+    if (params_acm%geometry == "two-moving-cylinders") use_color = .true.
 
 
 
@@ -407,6 +409,13 @@ subroutine STATISTICS_ACM( time, dt, u, g, x0, dx, stage, work, mask )
             call append_t_file( 'div.t', (/time, params_acm%div_max, params_acm%div_min/) )
             call append_t_file( 'forces.t', (/time, sum(params_acm%force_color(1,:)), &
             sum(params_acm%force_color(2,:)), sum(params_acm%force_color(3,:)) /) )
+
+            if (use_color) then
+                color = 1_2
+                call append_t_file( 'forces_1.t', (/time, params_acm%force_color(:,color)/) )
+                color = 2_2
+                call append_t_file( 'forces_2.t', (/time, params_acm%force_color(:,color)/) )
+            endif
 
             if (is_insect) then
                 call append_t_file( 'aero_power.t', (/time, apowtotal, ipowtotal/) )

75202af4c44df99ffe6bd54aaa04dec5d9c07d6d
Thomas Engels <thomas.engels@tnt.tu-berlin.de>
2025-03-21 14:58:05 +0100

    Wavelets: allow safetyZone without coarseExtension

--- LIB/MESH/adapt_tree.f90
+++ LIB/MESH/adapt_tree.f90
@@ -282,7 +282,7 @@ subroutine adapt_tree( time, params, hvy_block, tree_ID, indicator, hvy_tmp, hvy
         ! coarseningIndicator_tree works on all blocks (for wavelet cases).
         ! blocks that are significant now have status 0, others have -1
 
-        if (params%useSecurityZone .and. indicator/="everywhere" .and. indicator/="random" .and. params%useCoarseExtension) then
+        if (params%useSecurityZone .and. indicator/="everywhere" .and. indicator/="random") then
             ! if we want to add a security zone, we check for every significant block if a neighbor wants to coarsen
             ! if this is the case, we check if any significant WC would be deleted (basically checking the thresholding for this patch)
             ! in that case we set the neighbouring block to be important as well (with a temporary flag)
